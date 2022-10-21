d6f3f1240d75c45692407d7a63e821e019e916fe
Martinoux <martinblanckaert@yhaoo.fr>
2022-10-21 17:44:31 -0400

    trying shit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import chesslib.Board;
+import chesslib.move.Move;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Board board = new Board();
+        List<Move> moves = board.legalMoves();
+        System.out.println("Legal moves: " + moves);
     }
 }
