eec7e63149f75d7af334990c87952f51d26e08fe
Thomas Sirvent <sirventhomas@gmail.com>
2022-11-02 10:30:25 -0400

    Update UCI.java

--- src/UCI.java
+++ src/UCI.java
@@ -9,92 +9,83 @@ import java.util.*;
 public class UCI {
     static Board board = new Board();
 
-    static String ENGINENAME="Tomart1";
+    static String ENGINENAME = "Tomart1";
+
     public static void uciCommunication() {
         Scanner input = new Scanner(System.in);
-        while (true)
-        {
-            String inputString=input.nextLine();
-            if ("uci".equals(inputString))
-            {
+        while (true) {
+            String inputString = input.nextLine();
+            if ("uci".equals(inputString)) {
                 inputUCI();
-            }
-            else if (inputString.startsWith("setoption"))
-            {
+            } else if (inputString.startsWith("setoption")) {
                 inputSetOption(inputString);
-            }
-            else if ("isready".equals(inputString))
-            {
+            } else if ("isready".equals(inputString)) {
                 inputIsReady();
-            }
-            else if ("ucinewgame".equals(inputString))
-            {
+            } else if ("ucinewgame".equals(inputString)) {
                 inputUCINewGame();
-            }
-            else if (inputString.startsWith("position"))
-            {
+            } else if (inputString.startsWith("position")) {
                 inputPosition(inputString);
-            }
-            else if (inputString.startsWith("go"))
-            {
+            } else if (inputString.startsWith("go")) {
                 inputGo();
-            }
-            else if (inputString.equals("quit"))
-            {
+            } else if (inputString.equals("quit")) {
                 inputQuit();
-            }
-            else if ("print".equals(inputString))
-            {
+            } else if ("print".equals(inputString)) {
                 inputPrint();
             }
         }
     }
+
     public static void inputUCI() {
-        System.out.println("id name "+ENGINENAME);
+        System.out.println("id name " + ENGINENAME);
         System.out.println("id author Thomartin");
         //options go here
         System.out.println("uciok");
     }
+
     public static void inputSetOption(String inputString) {
         //set options
     }
+
     public static void inputIsReady() {
-         System.out.println("readyok");
+        System.out.println("readyok");
     }
+
     public static void inputUCINewGame() {
         board.loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     }
+
     public static void inputPosition(String input) {
-        input=input.substring(9).concat(" ");
+        input = input.substring(9).concat(" ");
         if (input.contains("startpos ")) {
-            input=input.substring(9);
+            input = input.substring(9);
             board.loadFromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-        }
-        else if (input.contains("fen")) {
-            input=input.substring(4);
+        } else if (input.contains("fen")) {
+            input = input.substring(4);
             board.loadFromFen(input);
         }
         if (input.contains("moves")) {
-            input=input.substring(input.indexOf("moves")+6);
+            input = input.substring(input.indexOf("moves") + 6);
             MoveList list = new MoveList();
             list.loadFromSan(input);
             board.loadFromFen(list.getFen());
         }
     }
+
     public static void inputGo() {
 
         //Minimax algo = new Minimax(board, 4);
         boolean isMax = board.getSideToMove() == Side.WHITE;
-        Node bestNode = Minimax.minimax(board, 5, -Double.MAX_VALUE, Double.MAX_VALUE, isMax);
+        Node bestNode = Minimax.minimax(board, 4, -Double.MAX_VALUE, Double.MAX_VALUE, isMax);
         Move bestMove = bestNode.move;
-        System.out.println("Nodes explored "+ Minimax.cpt);
-        System.out.println("bestmove "+ bestMove);
+        System.out.println("Nodes explored " + Minimax.cpt);
+        System.out.println("bestmove " + bestMove);
         Minimax.cpt = 0;
     }
 
     public static void inputQuit() {
         System.exit(0);
     }
+
     public static void inputPrint() {
 
     }
