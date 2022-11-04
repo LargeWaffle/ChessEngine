8d6166b7f7f084b66ace6c75c81461e908de462a
Thomas Sirvent <sirventhomas@gmail.com>
2022-11-04 12:14:23 -0400

    Pawn stuff

--- src/algorithms/Minimax.java
+++ src/algorithms/Minimax.java
@@ -4,6 +4,9 @@ import chesslib.*;
 import chesslib.move.*;
 
 import java.util.*;
+
+import static chesslib.Bitboard.bitScanForward;
+import static chesslib.Bitboard.extractLsb;
 import static java.lang.Long.bitCount;
 
 public class Minimax {
@@ -24,7 +27,7 @@ public class Minimax {
             {30, 31, 32, 33, 34, 35, 0}, // victim B, attacker K, Q, R, B, N, P, None
             {20, 21, 22, 23, 24, 25, 0}, // victim N, attacker K, Q, R, B, N, P, None
             {10, 11, 12, 13, 14, 15, 0}, // victim P, attacker K, Q, R, B, N, P, None
-            {0,  0,  0,  0,  0,  0,  0},      // no victim
+            {0, 0, 0, 0, 0, 0, 0},      // no victim
     };
     public static Map<Character, Double> toto_values = new HashMap<>(Map.of(
             'K', 200.0, 'Q', 9.0, 'R', 5.0, 'N', 3.0, 'B', 3.0, 'P', 1.0));
@@ -50,74 +53,74 @@ public class Minimax {
     public static double cpt = 0;
 
     public static List<Integer> pawnTable = Arrays.asList(
-            0,  0,  0,  0,  0,  0,  0,  0,
+            0, 0, 0, 0, 0, 0, 0, 0,
             50, 50, 50, 50, 50, 50, 50, 50,
             10, 10, 20, 30, 30, 20, 10, 10,
-            5,  5, 10, 25, 25, 10,  5,  5,
-            0,  0,  0, 20, 20,  0,  0,  0,
-            5, -5,-10,  0,  0,-10, -5,  5,
-            5, 10, 10,-20,-20, 10, 10,  5,
-            0,  0,  0,  0,  0,  0,  0,  0);
+            5, 5, 10, 25, 25, 10, 5, 5,
+            0, 0, 0, 20, 20, 0, 0, 0,
+            5, -5, -10, 0, 0, -10, -5, 5,
+            5, 10, 10, -20, -20, 10, 10, 5,
+            0, 0, 0, 0, 0, 0, 0, 0);
 
     public static List<Integer> knightTable = Arrays.asList(
-            -50,-40,-30,-30,-30,-30,-40,-50,
-            -40,-20,  0,  0,  0,  0,-20,-40,
-            -30,  0, 10, 15, 15, 10,  0,-30,
-            -30,  5, 15, 20, 20, 15,  5,-30,
-            -30,  0, 15, 20, 20, 15,  0,-30,
-            -30,  5, 10, 15, 15, 10,  5,-30,
-            -40,-20,  0,  5,  5,  0,-20,-40,
-            -50,-40,-30,-30,-30,-30,-40,-50);
+            -50, -40, -30, -30, -30, -30, -40, -50,
+            -40, -20, 0, 0, 0, 0, -20, -40,
+            -30, 0, 10, 15, 15, 10, 0, -30,
+            -30, 5, 15, 20, 20, 15, 5, -30,
+            -30, 0, 15, 20, 20, 15, 0, -30,
+            -30, 5, 10, 15, 15, 10, 5, -30,
+            -40, -20, 0, 5, 5, 0, -20, -40,
+            -50, -40, -30, -30, -30, -30, -40, -50);
 
     public static List<Integer> bishopTable = Arrays.asList(
-            -20,-10,-10,-10,-10,-10,-10,-20,
-            -10,  0,  0,  0,  0,  0,  0,-10,
-            -10,  0,  5, 10, 10,  5,  0,-10,
-            -10,  5,  5, 10, 10,  5,  5,-10,
-            -10,  0, 10, 10, 10, 10,  0,-10,
-            -10, 10, 10, 10, 10, 10, 10,-10,
-            -10,  5,  0,  0,  0,  0,  5,-10,
-            -20,-10,-10,-10,-10,-10,-10,-20);
+            -20, -10, -10, -10, -10, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 0, 5, 10, 10, 5, 0, -10,
+            -10, 5, 5, 10, 10, 5, 5, -10,
+            -10, 0, 10, 10, 10, 10, 0, -10,
+            -10, 10, 10, 10, 10, 10, 10, -10,
+            -10, 5, 0, 0, 0, 0, 5, -10,
+            -20, -10, -10, -10, -10, -10, -10, -20);
 
     public static List<Integer> rookTable = Arrays.asList(
-            0,  0,  0,  0,  0,  0,  0,  0,
-            5, 10, 10, 10, 10, 10, 10,  5,
-            -5,  0,  0,  0,  0,  0,  0, -5,
-            -5,  0,  0,  0,  0,  0,  0, -5,
-            -5,  0,  0,  0,  0,  0,  0, -5,
-            -5,  0,  0,  0,  0,  0,  0, -5,
-            -5,  0,  0,  0,  0,  0,  0, -5,
-            0,  0,  0,  5,  5,  0,  0,  0);
+            0, 0, 0, 0, 0, 0, 0, 0,
+            5, 10, 10, 10, 10, 10, 10, 5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            -5, 0, 0, 0, 0, 0, 0, -5,
+            0, 0, 0, 5, 5, 0, 0, 0);
 
     public static List<Integer> queenTable = Arrays.asList(
-            -20,-10,-10, -5, -5,-10,-10,-20,
-            -10,  0,  0,  0,  0,  0,  0,-10,
-            -10,  0,  5,  5,  5,  5,  0,-10,
-            -5,  0,  5,  5,  5,  5,  0, -5,
-            0,  0,  5,  5,  5,  5,  0, -5,
-            -10,  5,  5,  5,  5,  5,  0,-10,
-            -10,  0,  5,  0,  0,  0,  0,-10,
-            -20,-10,-10, -5, -5,-10,-10,-20);
+            -20, -10, -10, -5, -5, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 0, 5, 5, 5, 5, 0, -10,
+            -5, 0, 5, 5, 5, 5, 0, -5,
+            0, 0, 5, 5, 5, 5, 0, -5,
+            -10, 5, 5, 5, 5, 5, 0, -10,
+            -10, 0, 5, 0, 0, 0, 0, -10,
+            -20, -10, -10, -5, -5, -10, -10, -20);
 
     public static List<Integer> kingMiddleTable = Arrays.asList(
-            -20,-10,-10, -5, -5,-10,-10,-20,
-            -10,  0,  0,  0,  0,  0,  0,-10,
-            -10,  0,  5,  5,  5,  5,  0,-10,
-            -5,  0,  5,  5,  5,  5,  0, -5,
-            0,  0,  5,  5,  5,  5,  0, -5,
-            -10,  5,  5,  5,  5,  5,  0,-10,
-            -10,  0,  5,  0,  0,  0,  0,-10,
-            -20,-10,-10, -5, -5,-10,-10,-20);
+            -20, -10, -10, -5, -5, -10, -10, -20,
+            -10, 0, 0, 0, 0, 0, 0, -10,
+            -10, 0, 5, 5, 5, 5, 0, -10,
+            -5, 0, 5, 5, 5, 5, 0, -5,
+            0, 0, 5, 5, 5, 5, 0, -5,
+            -10, 5, 5, 5, 5, 5, 0, -10,
+            -10, 0, 5, 0, 0, 0, 0, -10,
+            -20, -10, -10, -5, -5, -10, -10, -20);
 
     public static List<Integer> kingEndingTable = Arrays.asList(
-            -50,-40,-30,-20,-20,-30,-40,-50,
-            -30,-20,-10,  0,  0,-10,-20,-30,
-            -30,-10, 20, 30, 30, 20,-10,-30,
-            -30,-10, 30, 40, 40, 30,-10,-30,
-            -30,-10, 30, 40, 40, 30,-10,-30,
-            -30,-10, 20, 30, 30, 20,-10,-30,
-            -30,-30,  0,  0,  0,  0,-30,-30,
-            -50,-30,-30,-30,-30,-30,-30,-50);
+            -50, -40, -30, -20, -20, -30, -40, -50,
+            -30, -20, -10, 0, 0, -10, -20, -30,
+            -30, -10, 20, 30, 30, 20, -10, -30,
+            -30, -10, 30, 40, 40, 30, -10, -30,
+            -30, -10, 30, 40, 40, 30, -10, -30,
+            -30, -10, 20, 30, 30, 20, -10, -30,
+            -30, -30, 0, 0, 0, 0, -30, -30,
+            -50, -30, -30, -30, -30, -30, -30, -50);
 
     public static Map<PieceType, List<Integer>> midgamePST = new HashMap<>(Map.of(
             PieceType.PAWN, pawnTable,
@@ -233,7 +236,7 @@ public class Minimax {
     public static double evaluate(Board board, boolean max, int phase, boolean draw, boolean mated, int playingMoveSize) {
 
         // EDGE CASES
-        double score = 0.0, materialScore = 0.0, controlScore = 0.0, mobilityScore = 0.0;
+        double score = 0.0, materialScore = 0.0, controlScore = 0.0, mobilityScore = 0.0, pawnScore = 0.0;
 
         if (mated)
             return max ? lowerBound : higherBound;
@@ -241,26 +244,77 @@ public class Minimax {
         if (draw)
             return 0.0;
 
-        int matW = 0, contW = 0, mobW = 0;
+        int matW = 0, contW = 0, mobW = 0, pawnW = 0;
+
         // BOARD ANALYSIS
         if (phase == 0) { // opening phase
             matW = 1;
             contW = 40;
             mobW = 2;
-        }
-        else if (phase == 1) { // middle phase
+            pawnW = 10;
+        } else if (phase == 1) { // middle phase
             matW = 1;
             contW = 40;
             mobW = 10;
-        }
-        else if (phase == 2) { // end phase
+            pawnW = 5;
+        } else if (phase == 2) { // end phase
             matW = 1;
             contW = 10;
             mobW = 2;
+            pawnW = 1;
         }
 
         // Material evaluation
         long bboard = board.getBitboard();
+
+        int w_doubled = 0, b_doubled = 0, w_blocked = 0, b_blocked = 0, w_isolated = 0, b_isolated = 0;
+
+        long copyBoard = bboard;
+
+        while (copyBoard != 0L) {
+            int sq = bitScanForward(copyBoard);
+            copyBoard = extractLsb(copyBoard);
+
+            Square currentSquare = Square.squareAt(sq);
+            Square[] sideSquares = currentSquare.getSideSquares();
+            Piece p = board.getPiece(currentSquare);
+
+            boolean pieceSideWhite = p.getPieceSide() == Side.WHITE;
+            Piece controlPiece = pieceSideWhite ? Piece.WHITE_PAWN : Piece.BLACK_PAWN;
+
+            int frontIndex = pieceSideWhite ? sq + 8 : sq - 8;
+            Square frontSquare = Square.squareAt(frontIndex);
+            Piece frontPiece = board.getPiece(frontSquare);
+
+            if (frontIndex >= 0 && frontIndex < 64) {
+                if (frontPiece == controlPiece)
+                    if (pieceSideWhite)
+                        w_doubled++;
+                    else
+                        b_doubled--;
+
+                if (frontPiece != Piece.NONE)
+                    if (pieceSideWhite)
+                        w_blocked++;
+                    else
+                        b_blocked--;
+            }
+
+            boolean isIsolated = true;
+
+            for (Square square : sideSquares)
+                if (board.getPiece(square) == controlPiece)
+                    isIsolated = false;
+
+            if (isIsolated)
+                if (pieceSideWhite)
+                    w_isolated++;
+                else
+                    b_isolated--;
+        }
+
+        pawnScore = w_doubled + b_doubled + w_blocked + b_blocked + w_isolated + b_isolated;
+
         for (int i = 0; i < 64; i++) {
             if (((1L << i) & bboard) != 0L) {
                 Piece p = board.getPiece(Square.squareAt(i));
@@ -268,7 +322,7 @@ public class Minimax {
                 if (p.getPieceSide() == Side.WHITE)
                     materialScore += pieceValues.get(type) + (phase == 1 ? midgamePST.get(type).get(i) : endgamePST.get(type).get(i));
                 else
-                    materialScore -= pieceValues.get(type) - (phase == 1 ? midgamePST.get(type).get(63-i) : endgamePST.get(type).get(63-i));
+                    materialScore -= pieceValues.get(type) - (phase == 1 ? midgamePST.get(type).get(63 - i) : endgamePST.get(type).get(63 - i));
             }
         }
 
@@ -290,7 +344,7 @@ public class Minimax {
             mobilityScore += MoveGenerator.getLegalMovesSize(board, Side.WHITE) - playingMoveSize;
 
         // Tapered evaluation
-        score = matW * materialScore + contW * controlScore + mobW * mobilityScore;
+        score = matW * materialScore + contW * controlScore + mobW * mobilityScore + pawnW * pawnScore;
 
         // scale between lowerBound and higherBound
         score = (score - lowerBound) / (higherBound - lowerBound);
@@ -304,8 +358,7 @@ public class Minimax {
                 return 1;
             else
                 return 0;
-        }
-        else // victim/attacker capture : second highest
+        } else // victim/attacker capture : second highest
             return vic_atk_val[pieceIndex.get(vic)][pieceIndex.get(atk)];
     }
 }
